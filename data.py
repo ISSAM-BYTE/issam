5e8805ccc765f1fd31a82adeec0f6b61ec0103d0
i.hafi <issamhefii@gmail.com>
2024-09-20 16:50:44 +0200

    modif

--- data.py
+++ data.py
@@ -1,2 +1,30 @@
 print('modifying data')
 print('super redesign')
+print('lollilop')
+
+def somme(a,b):
+    return a + b
+
+class high():
+
+    def __init__(self):
+
+        self.__a = 0
+
+        self.__b = 5
+
+    def higher(self):
+        print('higher')
+
+    def file(self):
+        with open("test.txt","w+") as file :
+
+           file.write('issam')
+
+
+c = somme(5,7)
+h = high()
+h.higher()
+h.file()
+print(h.__b)
+
